b8e66181dba5b89535f1f7def33865604a33b61d
wangxiaoyu006 <wangxiaoyu006@chinasofti.com>
2024-05-28 21:49:58 +0800

    日志优化

--- src/MyLogger.ts
+++ src/MyLogger.ts
@@ -1,4 +1,6 @@
 import { LoggerService, LogLevel } from '@nestjs/common';
+import * as chalk from 'chalk';
+import * as dayjs from 'dayjs';
 import { createLogger, format, Logger, transports } from 'winston';
 
 export class MyLogger implements LoggerService {
@@ -6,19 +8,34 @@ export class MyLogger implements LoggerService {
   constructor() {
     this.logger = createLogger({
       level: 'debug',
-      format: format.combine(format.colorize(), format.simple()),
+      format: format.combine(
+        format.colorize(),
+        format.printf(({ context, level, message, time }) => {
+          const appStr = chalk.green(`[NEST]`);
+          const contextStr = chalk.yellow(`[${context}]`);
+
+          return `${appStr} ${time} ${level} ${contextStr} ${message} `;
+        }),
+        //   format.simple()
+      ),
       transports: [new transports.Console()],
     });
   }
   log(message: string, context: string) {
-    this.logger.log('info', `[${context}] ${message}`);
+    const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+
+    this.logger.log('info', message, { context, time });
   }
 
   error(message: string, context: string) {
-    this.logger.log('error', `[${context}] ${message}`);
+    const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+
+    this.logger.log('info', message, { context, time });
   }
 
   warn(message: string, context: string) {
-    this.logger.log('warn', `[${context}] ${message}`);
+    const time = dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+
+    this.logger.log('info', message, { context, time });
   }
 }
