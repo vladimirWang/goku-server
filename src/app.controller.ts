4cb2e14c77f4fac6fbaf6bfcc52245f3eea26725
wangxiaoyu006 <wangxiaoyu006@chinasofti.com>
2024-05-28 21:28:30 +0800

    MyLogger基础实现

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  private logger = new Logger();
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    this.logger.log('hello', AppController.name);
     return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MyLogger } from './MyLogger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useLogger(new MyLogger());
   await app.listen(3000);
 }
 bootstrap();
