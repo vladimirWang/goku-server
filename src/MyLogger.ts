e177cab36087fbf22174c1c76bac19b783b0fe49
wangxiaoyu006 <wangxiaoyu006@chinasofti.com>
2024-05-28 21:33:43 +0800

    用winston代替console

--- src/MyLogger.ts
+++ src/MyLogger.ts
@@ -1,15 +1,24 @@
 import { LoggerService, LogLevel } from '@nestjs/common';
+import { createLogger, format, Logger, transports } from 'winston';
 
 export class MyLogger implements LoggerService {
+  private logger: Logger;
+  constructor() {
+    this.logger = createLogger({
+      level: 'debug',
+      format: format.combine(format.colorize(), format.simple()),
+      transports: [new transports.Console()],
+    });
+  }
   log(message: string, context: string) {
-    console.log(`---log---[${context}]---`, message);
+    this.logger.log('info', `[${context}] ${message}`);
   }
 
   error(message: string, context: string) {
-    console.log(`---error---[${context}]---`, message);
+    this.logger.log('error', `[${context}] ${message}`);
   }
 
   warn(message: string, context: string) {
-    console.log(`---warn---[${context}]---`, message);
+    this.logger.log('warn', `[${context}] ${message}`);
   }
 }
